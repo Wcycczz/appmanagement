7ff45b00349e6c46632bd1bbee64e0fcedc54bfa
niudq <niudq@geeferri.com>
2018-09-18 11:18:59 +0800

    1

---
+++ src/main/java/com/ruiliang/management/controller/VersionController.java
@@ -0,0 +1,185 @@
+package com.ruiliang.management.controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.ruiliang.management.constants.Constants;
+import com.ruiliang.management.pojo.Manager;
+import com.ruiliang.management.pojo.UserInfo;
+import com.ruiliang.management.pojo.Version;
+import com.ruiliang.management.service.VersionService;
+import com.ruiliang.management.upload.FileUploadService;
+import com.ruiliang.management.util.CharUtils;
+import com.ruiliang.management.util.JSON;
+import com.ruiliang.management.util.MD5Util;
+
+
+@Controller
+@RequestMapping("version")
+public class VersionController {
+	
+	private Logger LOG = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	private FileUploadService fs;
+	@Autowired
+	private VersionService vs;
+	
+	@RequestMapping("toVersionList")
+	public String toVersionList(){
+		return "admin/version/versionList";
+	}
+	
+	@RequestMapping("getVersion")
+	@ResponseBody
+	public String getVersion(HttpServletRequest request){
+		String key = request.getParameter("key");
+		String pageIndex = request.getParameter("pageIndex");
+		String pageSize = request.getParameter("pageSize");
+		int count = vs.getCount(key);
+		List<Version> users = vs.selectVersion(key,Integer.parseInt(pageIndex),Integer.parseInt(pageSize));
+		for (Version version : users) {
+			if(version.getForceflag() == 1){
+				version.setFf("否");
+			}
+			if(version.getForceflag() == 2){
+				version.setFf("是");
+			}
+			if(version.getPlatForm()==1){
+				version.setPf("安卓");
+			}
+			if(version.getPlatForm()==2){
+				version.setPf("苹果");
+			}
+		}
+		Map<String,Object> map  = new HashMap<String, Object>();
+		map.put("data", users);
+		map.put("total",count);
+		String string = JSON.encode(map);
+		LOG.info("版本数据=====>"+string);
+		return string;
+	}
+	
+	@RequestMapping("toVersion")
+	public String toUser(){
+		return "admin/version/version";
+	}
+	
+	/*@RequestMapping("getCus")
+	@ResponseBody
+	public ArrayList<Customer> getCus(){
+		ArrayList<Customer> list = (ArrayList<Customer>)cs.selectAllCustomer();
+		return list;
+	}
+	*/
+	@RequestMapping("saveVersion")
+	public String saveVersion(@RequestParam("ui") String content,Model model,HttpServletRequest request,@RequestParam("file") MultipartFile file,@ModelAttribute Version v){
+		if(StringUtils.isNoneBlank(file.getOriginalFilename())){
+			String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+			if(!Objects.equals("apk", suffix)){
+				model.addAttribute("msg", "必须是APK格式");
+				model.addAttribute("code",-1);
+				return "admin/frame/result";
+			}
+			String apk = "";
+			try {
+				InputStream	inputStream = new ByteArrayInputStream(file.getBytes());
+				
+				try {
+					 apk = fs.copy2Apk(inputStream, suffix, "");
+				} catch (Exception e) {
+					model.addAttribute("msg", "APK上传错误");
+					model.addAttribute("code",-1);
+					return "admin/frame/result";
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			v.setDownUrl(apk);
+		}
+		v.setUpdateInfo(content);
+		v.setcTime(new Date());
+		if(v.getId() == null){
+			
+			int i = vs.insert(v);
+			if(i != 1){
+				model.addAttribute("msg", "保存错误");
+				model.addAttribute("code",-1);
+				return "admin/frame/result";
+			}
+		}else{
+			int i = vs.update(v);
+			if(i != 1){
+				model.addAttribute("msg", "更新错误");
+				model.addAttribute("code",-1);
+				return "admin/frame/result";
+			}
+		}
+		
+		model.addAttribute("msg", "保存成功");
+		model.addAttribute("code",0);
+		return "admin/frame/result";
+	}
+	
+	@RequestMapping("getVersionByCid")
+	@ResponseBody
+	public HashMap getUserByCid(HttpServletRequest request) throws Exception{
+		String id = request.getParameter("id");
+		Version ui = vs.selectVersionByid(id);
+		HashMap<String, Object> toMap = convertToMap(ui);
+		return toMap;
+	}
+	
+	@RequestMapping("deleteVersionByids")
+	@ResponseBody
+	public void deleteUserByids(HttpServletRequest request) throws Exception{
+		String id = request.getParameter("id");
+		String[] ids = id.split(",");
+		int[] arr = new int[ids.length];
+		for(int i=0; i< ids.length; i++){
+			arr[i] = Integer.parseInt(ids[i]);
+		}
+		vs.deleteVersion(arr);
+	}
+	private  HashMap<String, Object> convertToMap(Object obj)
+            throws Exception {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (int i = 0, len = fields.length; i < len; i++) {
+            String varName = fields[i].getName();
+            boolean accessFlag = fields[i].isAccessible();
+            fields[i].setAccessible(true);
+
+            Object o = fields[i].get(obj);
+            if (o != null)
+                map.put(varName, o.toString());
+
+            fields[i].setAccessible(accessFlag);
+        }
+        return map;
+    }
+	
+}
---
+++ src/main/java/com/ruiliang/management/pojo/Version.java
@@ -0,0 +1,163 @@
+package com.ruiliang.management.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author LinJian.Liu
+ * 版本
+ */
+public class Version implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6226058384664244252L;
+
+	/**
+	 * 主键
+	 */
+	private Integer id;
+	
+	/**
+	 * 平台 1安卓 2IOS
+	 */
+	private Byte platForm;
+	
+	private String pf;
+	
+	/**
+	 * 版本号
+	 */
+	private Integer code;
+	
+	/**
+	 * 版本信息
+	 */
+	private String info;
+	
+	/**
+	 * APK 路径
+	 */
+	private String downUrl;
+	
+	/**
+	 * 更新内容信息
+	 */
+	private String updateInfo;
+	
+	/**
+	 * 创建时间
+	 */
+	private Date cTime;
+	
+	/**
+	 * 是否强更 1 否 2是
+	 */
+	private Byte forceflag;
+	
+	private String ff;
+	
+	
+
+	public String getPf() {
+		return pf;
+	}
+
+	public void setPf(String pf) {
+		this.pf = pf;
+	}
+
+	public Byte getForceflag() {
+		return forceflag;
+	}
+
+	public void setForceflag(Byte forceflag) {
+		this.forceflag = forceflag;
+	}
+
+	public String getFf() {
+		return ff;
+	}
+
+	public void setFf(String ff) {
+		this.ff = ff;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Byte getPlatForm() {
+		return platForm;
+	}
+
+	public void setPlatForm(Byte platForm) {
+		this.platForm = platForm;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getDownUrl() {
+		return downUrl;
+	}
+
+	public void setDownUrl(String downUrl) {
+		this.downUrl = downUrl;
+	}
+
+	public String getUpdateInfo() {
+		return updateInfo;
+	}
+
+	public void setUpdateInfo(String updateInfo) {
+		this.updateInfo = updateInfo;
+	}
+
+	public Date getcTime() {
+		return cTime;
+	}
+
+	public void setcTime(Date cTime) {
+		this.cTime = cTime;
+	}
+
+	
+	public Byte getForceFlag() {
+		return forceflag;
+	}
+
+	public void setForceFlag(Byte forceflag) {
+		this.forceflag = forceflag;
+	}
+
+	@Override
+	public String toString() {
+		return "Version [id=" + id + ", platForm=" + platForm + ", code="
+				+ code + ", info=" + info + ", downUrl=" + downUrl
+				+ ", updateInfo=" + updateInfo + ", cTime=" + cTime
+				+ ", forceflag=" + forceflag + "]";
+	}
+
+	
+	
+	
+}
